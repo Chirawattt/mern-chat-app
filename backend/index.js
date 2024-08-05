0e19656dfd5ede7a2560a587a8f065252f9d1c13
Chirawattt <chirawat.yana@hotmail.com>
2024-08-05 13:36:31 +0700

    update package.json of the root folder

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -9,6 +10,8 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json()); // to parse incoming requests with JSON payloads (from req.body) - [middleware]
@@ -22,6 +25,11 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
